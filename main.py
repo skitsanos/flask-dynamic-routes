4291b26889649df26c95ad999bec39c1cea86961
skitsanos <info@skitsanos.com>
2023-04-18 11:56:17 +0300

    chore: core functionality

--- main.py
+++ main.py
@@ -6,11 +6,20 @@ def load_routes(app, path):
     for root, dirs, files in os.walk(path):
         for dir in dirs:
             route_path = os.path.relpath(os.path.join(root, dir), path)
+            module_path = os.path.relpath(os.path.join(root, dir), path)
+
+            # Check if dir starts with $
+            if dir.startswith("$"):
+                # Replace $ with < and > to make a path parameter
+                route_path = os.path.relpath(os.path.join(root, f"<{dir.replace('$', '')}>"), path)
+                print(route_path)
+                print(module_path)
+
             methods = []
             for method in ("get", "post", "put", "delete"):
                 if os.path.exists(os.path.join(root, dir, f"{method}.py")):
                     methods.append(method.upper())
-                    module_name = f"routes.{route_path.replace(os.sep, '.')}.{method}"
+                    module_name = f"routes.{module_path.replace(os.sep, '.')}.{method}"
 
                     module = __import__(module_name, fromlist=["*"])
                     app.add_url_rule(f"/{route_path}",
---
+++ routes/api/users/$user_id/get.py
@@ -0,0 +1,4 @@
+def handler(user_id):
+    return {
+        "info": f"Get User by ID #{user_id}"
+    }
